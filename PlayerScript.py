b3277fef41fdee889ec9d1150a41575a5c370729
Demented <115271904+Demented-101@users.noreply.github.com>
2025-02-07 12:19:24 +0000

    Finished Player for sprint 1
    no testing done just yet. added bankruptcy, improved jail and payment systems, and fixed some of the space actions being incorrect.

--- PlayerScript.py
+++ PlayerScript.py
@@ -2,27 +2,33 @@ from __future__ import annotations
 import Spaces as space
 import random as ran
 
-## hrllo testing
+## PROMPTS NEEDED:
+    # purchase property
+    # leave Jail (50 payment)
+    # upgrade property prompt
 
 class player:
     player_num:int = 0
+    is_bankrupt:bool = False
     position:int = 0
-    money:int = 0
+    money:int = 1500
     properties:list
     
     in_jail:bool = False
     jail_turns:int = 0
     GOOJ_cards:list[bool] ## true is opp, false is pot
     
-    def roll(self):
+    def roll(self) -> None:
+        if self.is_bankrupt: return ## nullify turn if bankrupt/not in game
+        
         if self.in_jail:
             if self.attempt_jail_leave():
                 self.in_jail = False
-                self.position = 20 ## i think this is just visiting??
+                self.position = 10 ## i think this is just visiting??
                 ## move peice
                 ## wait time
             else:
-                return
+                return ## return so that the rest of the turn is nullified
 
         roll = ran.randint(2,12)
         for i in range(roll):
@@ -39,25 +45,26 @@ class player:
             case 2: pass ## TODO - take parking
             case 3: self.pay_parking(200)
             case 4: self.pay_parking(100)
-            case 5: pass ## TODO - go to jail
+            case 5: self.go_to_jail()
         
+        ## TODO - prompt purchase
         ## TODO - prompt upgrade selection
                 
-    def attempt_pay(self, amount:int) -> bool:
+    def attempt_pay(self, amount:int) -> int:
         if amount > self.money: 
-             ## cannot pay, TODO - add property selling and bankrupcy
-            return False
+            self.is_bankrupt = True
+            prev_money = self.money
+            self.money = 0
+            return prev_money
         else:
             self.money -= amount
-            return True
+            return amount
     
     def pay_parking(self, amount:int) -> None:
-        if not self.attempt_pay(amount) : return
-        ## add money to parking variable
+        to_pay_middle = self.attempt_pay(amount) ## TODO - add to free parking total
     
     def pay_player(self, to:player, amount:int) -> None:
-        if not self.attempt_pay(amount) : return
-        to.money += amount
+        to.money += self.attempt_pay(amount)
 
     def pull_card_opp(self) -> None: pass
     def pull_card_pot(self) -> None: pass
@@ -68,15 +75,14 @@ class player:
         ## move peice to jail
     
     def attempt_jail_leave(self) -> bool:
-        return False
+        if self.jail_turns == 3:
+            return True
+        
+        if self.attempt_pay(50):
+            return True ## TODO - prompt choice for this
+        
+        if len(self.GOOJ_cards) > 0:
+            card = self.GOOJ_cards.pop()
+            return True
 
-_player:player = player()
-run:bool = True
-while run:
-    _input:str = input("END: ")
-    if _input.upper() == "END": run = False
-    else:
-        _player.roll()
-        print("position: " + str(_player.position))
-        print("money: " + str(_player.money))
-
+        self.jail_turns += 1
